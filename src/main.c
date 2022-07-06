63256a22098a4295f0497365aec039dfbaa017be
Matthew Keller <matthewscottkeller@gmail.com>
2022-07-05 21:53:58 -0500

    early game loop

--- src/main.c
+++ src/main.c
@@ -27,20 +27,26 @@
 #include "Aldous-Broder.h"
 #include "Wilsons.h"
 #include "graphics.h"
+#include "input.h"
 
 enum{MENU, GENERATE, PLAY, QUIT};
 
 struct game{
     maze_t maze;
     char state;
+    uint8_t cellSize;
     uint8_t bgColor, wallColor, playerColor, goalColor;
 };
 
-
+struct player{
+    uint8_t row, col;
+};
 
 int main(void){
     static struct game game;
+    struct player player;
     ti_var_t file;
+    gfx_Begin();
     if(!(file = ti_Open("EMSETT", "r"))){
         game.bgColor = 255;
         game.wallColor = 0;
@@ -56,23 +62,63 @@ int main(void){
         ti_Read(&game.maze.rows, 1, 1, file);
         ti_Read(&game.maze.cols, 1, 1, file);
     }
-    gfx_Begin();
-    walled_maze(&game.maze);
-    clear_visited(&game.maze);
+    game.state = GENERATE;
     timer_Disable(1);
-    timer_Set(1, 0);
-    timer_Enable(1, TIMER_32K, TIMER_0INT, TIMER_UP);
-    int numToVisit = AB_gen(&game.maze, 0, 0);
-    numToVisit = wilsons_gen(&game.maze, numToVisit);
-    timer_Disable(1);
-    gfx_dispMaze(&game.maze, game.bgColor, game.wallColor, game.playerColor);
-    real_t elapsed_real;
-    char str[10];
-    elapsed_real = os_FloatToReal((float)timer_GetSafe(1, TIMER_UP) / 32768 <= 0.001f ? 0.0f : (float)timer_GetSafe(1, TIMER_UP) / 32768);
-    os_RealToStr(str, &elapsed_real, 8, 1, 2);
-    gfx_PrintString(" Time: ");
-    gfx_PrintString(str);
-    while(!os_GetCSC());
+    timer_SetReload(1, 32768 / 5);
+    timer_Set(1, 32768 / 5);
+    srand(rtc_Time());
+
+    while(game.state != QUIT){
+        if(game.state == MENU){
+
+        }else if(game.state == GENERATE){
+            walled_maze(&game.maze);
+            clear_visited(&game.maze);
+            int numToVisit = AB_gen(&game.maze, 0, 0);
+            numToVisit = wilsons_gen(&game.maze, numToVisit);
+            if(numToVisit == 0) game.state = PLAY;
+            player.row = player.col = 0;
+            game.cellSize = (((MAZE_Y_MAX - MAZE_Y_MIN)) / game.maze.rows < ((MAZE_X_MAX - MAZE_X_MIN))  / game.maze.cols ? 
+                (MAZE_Y_MAX - MAZE_Y_MIN) / game.maze.rows : 
+                (MAZE_X_MAX - MAZE_X_MIN) / game.maze.cols);
+            gfx_dispMaze(&game.maze, game.bgColor, game.wallColor, game.playerColor, game.cellSize);
+            continue;
+        }else if(game.state == PLAY){
+            int8_t moveDir = getMoveDir();
+            if(moveDir == -1){
+                continue;
+            }
+            gfx_SetColor(game.bgColor);
+            gfx_FillRectangle(MAZE_X_MIN + player.col * game.cellSize + 1, MAZE_Y_MIN + player.row * game.cellSize + 1, game.cellSize - 1, game.cellSize - 1);
+
+            switch(moveDir){
+                case North:
+                    if(player.row > 0 && !game.maze.cells[player.row - 1][player.col].south){
+                        player.row--;
+                    }
+                    break;
+                case East:
+                    if(player.col < game.maze.cols && !game.maze.cells[player.row][player.col].east){
+                        player.col++;
+                    }
+                    break;
+                case South:
+                    if(player.row < game.maze.rows && !game.maze.cells[player.row][player.col].south){
+                        player.row++;
+                    }
+                    break;
+                case West:
+                    if(player.col > 0 && !game.maze.cells[player.row][player.col - 1].east){
+                        player.col--;
+                    }
+                    break;
+
+            }
+            gfx_SetColor(game.playerColor);
+            gfx_FillRectangle(MAZE_X_MIN + player.col * game.cellSize + 1, MAZE_Y_MIN + player.row * game.cellSize + 1, game.cellSize - 1, game.cellSize - 1);
+        }
+    }
+    
     gfx_End();
     return 0;
 }
