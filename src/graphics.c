67ef86395406f40b659adbf238dccdbc8ff85aed
Matthew Keller <matthewscottkeller@gmail.com>
2022-08-17 22:25:12 -0500

    added framerate, graphics initializer, and play phase renderer

--- src/graphics.c
+++ src/graphics.c
@@ -1,15 +1,31 @@
 #include <tice.h>
 #include <stdint.h>
 #include <graphx.h>
+#include <debug.h>
 
 #include "graphics.h"
 
-void waitNextFrame(int target){
-    
-    while(544 > timer_GetSafe(1, TIMER_UP));
-    timer_Disable(1);
-    timer_Set(1, 0);
-    timer_Enable(1, TIMER_32K, TIMER_0INT, TIMER_UP);
+#define FRAME_LIMIT 60
+#define NUM_FRAMES_PER_MOVE 4
+
+void init_graphics(){
+    gfx_Begin();
+    gfx_SetDrawBuffer();
+
+    // Set up timer for maximum framerate
+    timer_Set(1, 32678 / FRAME_LIMIT);
+    timer_SetReload(1, 32678 / FRAME_LIMIT);
+    timer_Enable(1, TIMER_32K, TIMER_0INT, TIMER_DOWN);
+}
+
+/**
+ * @brief Waits until enough time has elapsed since previous frame for next one.
+ * 
+ */
+void waitNextFrame(){
+    while(!timer_ChkInterrupt(1, TIMER_RELOADED));
+    timer_Set(1, 32678 / FRAME_LIMIT);
+    timer_AckInterrupt(1, TIMER_RELOADED);
     return;
 }
 
@@ -36,9 +52,37 @@ void gfx_dispMaze(maze_t *maze, uint8_t bgColor, uint8_t wallColor, uint8_t play
     gfx_PrintInt(maze->cols, 1);
     gfx_PrintString(" Height: ");
     gfx_PrintInt(maze->rows, 1);
+    gfx_BlitBuffer();
 
 }
 
 void render_play(struct game_s *game, struct player_s *player){
+
+    int playerEndX = MAZE_X_MIN + player->col * game->cellSize + 1;
+    int playerEndY = MAZE_Y_MIN + player->row * game->cellSize + 1;
+
+    // Not initialized as steps. Used to find starting position before division due to 
+    // possible pixel loss when dividing
+    int8_t xStep = game->cellSize * ((player->moveDir == East) - (player->moveDir == West));
+    int8_t yStep = game->cellSize * ((player->moveDir == South) - (player->moveDir == North));
+
+    // Clear player from screen at start of frame
+    gfx_SetColor(game->bgColor);
+    gfx_FillRectangle(playerEndX - xStep, playerEndY - yStep, game->cellSize - 1, game->cellSize - 1);
+
+    // Set pixel steps to be... well... actual steps
+    xStep /= NUM_FRAMES_PER_MOVE;
+    yStep /= NUM_FRAMES_PER_MOVE;
     
+    for(uint8_t i = NUM_FRAMES_PER_MOVE + 1; i > 0; i--){
+        gfx_SetColor(game->bgColor);
+        gfx_FillRectangle(playerEndX - xStep * i, playerEndY - yStep * i, game->cellSize - 1, game->cellSize - 1);
+        gfx_SetColor(game->playerColor);
+        gfx_FillRectangle(playerEndX - xStep * (i - 1), playerEndY - yStep * (i - 1), game->cellSize - 1, game->cellSize - 1);
+        waitNextFrame();
+        gfx_SwapDraw();
+        gfx_BlitScreen();
+    }
+    
+
 }
